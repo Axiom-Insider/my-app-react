10a9d0dc2c5f8a19e2a4cbf7c962fafd77adfe0f
axiom-insider <franciscomartins0006@gmail.com>
2025-04-24 12:30:09 -0300

    home terminado

--- my-app/src/pages/Funcionario/Home/Home.jsx
+++ my-app/src/pages/Funcionario/Home/Home.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavbarFuncionario from '../../../components/Navbar/NavbarFuncionario'
 import "./Home.css"
 import { IoPersonCircleSharp } from "react-icons/io5";
 
 export default function HomeFuncionario() {
+
+  const [data, setData] = useState(new Date())
+
   return (
     <div>
       <NavbarFuncionario />
       <div className="d-flex justify-content-center align-items-center">
         <div className="row">
           <div className="col">
-            <div className="date"><strong>Data:</strong> 04/09/2025</div>
+            <div className="date"><strong>Data:</strong>{data.toLocaleDateString("pt-BR")}</div>
             <div className="box">
               <div className="head">
                 <span className='avatar'> <IoPersonCircleSharp/> </span>
                 <span className="nome">Francisco Martins Gonçalves Gomes</span>
               </div>
               <div className="body">
-                <p><span className="matricula"><strong>Maticula: </strong>12085</span> <span className='entrada'><strong>Entrada: </strong> 12:09 </span></p>
-                <p><span className="cargo"><strong>Cargo: </strong>Pintor</span> <span className='saida'><strong>Saída: </strong> 17:09 </span></p>                
+                <div className='caixa' ><div className="matricula"><strong>Maticula:</strong>12085</div> <div className="cargo"><strong>Cargo:</strong>Pintor</div></div>
+                <div className="caixa"><div className='entrada'><strong>Entrada:</strong>12:09 </div> <div className='saida'><strong>Saída:</strong>17:09 </div></div>             
               </div>
             </div>
           </div>
