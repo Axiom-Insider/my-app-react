e7a67d86c9fa5f0097526b1237443ec93e13dc70
Axiom-Insider <franciscomartins0006@gmail.com>
2025-04-23 21:02:05 -0300

    pc de casa

--- my-app/src/pages/Funcionario/Home/Home.jsx
+++ my-app/src/pages/Funcionario/Home/Home.jsx
@@ -7,10 +7,20 @@ export default function HomeFuncionario() {
   return (
     <div>
       <NavbarFuncionario />
-      <div className="container">
+      <div className="d-flex justify-content-center align-items-center">
         <div className="row">
-          <div className="col-4">
-           <div className="box"></div>
+          <div className="col">
+            <div className="date"><strong>Data:</strong> 04/09/2025</div>
+            <div className="box">
+              <div className="head">
+                <span className='avatar'> <IoPersonCircleSharp/> </span>
+                <span className="nome">Francisco Martins Gonçalves Gomes</span>
+              </div>
+              <div className="body">
+                <p><span className="matricula"><strong>Maticula: </strong>12085</span> <span className='entrada'><strong>Entrada: </strong> 12:09 </span></p>
+                <p><span className="cargo"><strong>Cargo: </strong>Pintor</span> <span className='saida'><strong>Saída: </strong> 17:09 </span></p>                
+              </div>
+            </div>
           </div>
         </div>
       </div>
