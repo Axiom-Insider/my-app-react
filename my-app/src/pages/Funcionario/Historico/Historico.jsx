0d6379e3423a9522ab6d404a17ef9d57f159a30e
axiom-insider <franciscomartins0006@gmail.com>
2025-05-09 11:19:33 -0300

    historico terminado

--- my-app/src/pages/Funcionario/Historico/Historico.jsx
+++ my-app/src/pages/Funcionario/Historico/Historico.jsx
@@ -1,5 +1,6 @@
 import NavbarFuncionario from '../../../components/Navbar/NavbarFuncionario'
 import "./Historico.css"
+import usuarios from './usuarios';
 
 export default function Historico() {
   const mes = [
@@ -7,12 +8,13 @@ export default function Historico() {
     "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
   ];
 
+  const dados = usuarios();
+
   return (
     <div>
       <NavbarFuncionario />
       <div className="container d-flex justify-content-center align-items-center">
         <div className="box-historico">
-        
           <div className="row">
             <div className="col">
               <select class="form-select" id="mes" aria-label="Default select example">
@@ -21,13 +23,34 @@ export default function Historico() {
                   ))}
               </select>
             </div>
-            <div className="col">
-              <select class="form-select" id="mes" aria-label="Default select example">
-                {mes.map((nome, index) =>(
-                  <option value={index}>{nome}</option>
-                ))}
+            <div className="col-10">
+              <select className="form-select" id="ano" aria-label="Default select example">
+              <option value="2024" selected>2025</option>
+                <option value="2024">2024</option>
                 </select>
             </div>
+          </div>
+          <div className="row mt-4">
+            <table className='tabela'>
+              <thead>
+                <tr>
+                  <th>Data</th>
+                  <th>Feriado</th>
+                  <th>Horário de Entrada</th>
+                  <th>Horário de Saída</th>
+                </tr>
+              </thead>
+              <tbody>
+                {dados.map((dados)=>( 
+                  <tr key={dados.id}>
+                      <td>{dados.data}</td>
+                      <td>{dados.feriados}</td>
+                      <td>{dados.hora_entrada}</td>
+                      <td>{dados.hora_saida}</td>    
+                  </tr>
+                  ))}
+              </tbody>
+            </table>
           </div>
         </div>
         
