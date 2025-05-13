5ca4b60561654994a9b3798bc8ef11cb62849796
Axiom-Insider <franciscomartins0006@gmail.com>
2025-05-13 16:21:41 -0300

    começando o home

--- my-app/src/pages/Administrador/Home/Home.jsx
+++ my-app/src/pages/Administrador/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { FaUserTie } from "react-icons/fa"
 import NavbarAdm from "../../../components/Navbar/NavbarAdm"
 import "./Home.css"
 
@@ -7,11 +8,19 @@ function App() {
   return (
     <div>
       <NavbarAdm />
-      <div className="d-flex justify-content-center align-items-center">
-        <div className="row">
-        <div className="titulo">Funcionarios que já bateram ponto</div>
-          <div className="col-5">
-            <div className="alertas">Francisco Martins Gonçalves Gomes - <strong>Entrada:</strong> 12:03 | <strong>Saída:</strong> 17:03</div>
+      <div className="container d-flex justify-content-center align-items-center">
+        <div className="alertas">
+         <div className="row">
+          <div className="col-10">
+            <div className="titulo-home">Funcionarios que já bateram ponto</div>
+          </div>
+          <div className="col">
+            <div className="data">12/03/2030</div>
+          </div>
+         </div>
+          <div className="linha">
+            <div className="icone"><FaUserTie /></div> 
+            <div className="info"> Francisco Martins Gonçalves Gomes - <strong>Entrada:</strong> 12:03 | <strong>Saída:</strong> 17:03</div>
           </div>
         </div>
       </div>
