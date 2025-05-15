b732560e637ea4a1f2fcf2e6e6d80ca7f0734476
axiom-insider <franciscomartins0006@gmail.com>
2025-05-15 12:37:08 -0300

    home administrador quase terminado

--- my-app/src/pages/Administrador/Home/Home.jsx
+++ my-app/src/pages/Administrador/Home/Home.jsx
@@ -1,6 +1,7 @@
-import { FaUserTie } from "react-icons/fa"
+import { FaClock, FaRegClock, FaUserTie } from "react-icons/fa"
 import NavbarAdm from "../../../components/Navbar/NavbarAdm"
 import "./Home.css"
+import { CiClock2 } from "react-icons/ci"
 
 
 function App() {
@@ -10,16 +11,30 @@ function App() {
       <NavbarAdm />
       <div className="container d-flex justify-content-center align-items-center">
         <div className="alertas">
-         <div className="row">
-          <div className="col-10">
-            <div className="titulo-home">Funcionarios que já bateram ponto</div>
+          <div className="head-home">
+            <div className="clock"><FaRegClock /> </div>
+            <div className="titulo-home">
+              Funcionários que já bateram o ponto
+            </div>
+            <div className="data">12/03/2025</div>
           </div>
-          <div className="col">
-            <div className="data">12/03/2030</div>
-          </div>
-         </div>
           <div className="linha">
-            <div className="icone"><FaUserTie /></div> 
+            <div className="grupo">
+              <div className="icone">FM</div> 
+              <div className="info"> Francisco Martins Gonçalves Gomes</div> 
+            </div>
+             <div className="horarios"><strong>Entrada:</strong> 12:03 | <strong>Saída:</strong> 17:03</div> 
+          </div>
+          <div className="linha">
+            <div className="icone">FM</div> 
+            <div className="info"> Francisco Martins Gonçalves Gomes - <strong>Entrada:</strong> 12:03 | <strong>Saída:</strong> 17:03</div>
+          </div>
+          <div className="linha">
+            <div className="icone">FM</div> 
+            <div className="info"> Francisco Martins Gonçalves Gomes - <strong>Entrada:</strong> 12:03 | <strong>Saída:</strong> 17:03</div>
+          </div>
+          <div className="linha">
+            <div className="icone">FM</div> 
             <div className="info"> Francisco Martins Gonçalves Gomes - <strong>Entrada:</strong> 12:03 | <strong>Saída:</strong> 17:03</div>
           </div>
         </div>
